030a3f66cdd18bb03eed0f6d4d019907d1c9e815
Aditya <aditya.kw90@gmail.com>
2026-06-24 12:45:53 +0700

    core domain error

--- internal/core/domain/errors/general.go
+++ internal/core/domain/errors/general.go
@@ -1,16 +1,47 @@
 package errors
 
+const (
+	// error code
+	ErrCodeInternalServerError     = 10001
+	ErrCodeTraceInformationMissing = 10002
+	ErrCodeRequestCanceled         = 10003
+	ErrCodeRequestTimeout          = 10004
+	ErrCodeRequestAborted          = 10005
+	ErrCodeUnimplemented           = 10006
+	ErrCodeNotFound                = 10007
+	ErrCodeInvalidArgument         = 10008
+	ErrCodeValidation              = 10009
+	ErrCodePermissionDenied        = 10010
+	ErrCodeResourceConflict        = 10011
+	ErrCodeInvalidEntity           = 10012
+
+	// error message
+	ErrMessageInternalServerError     = "internal server error"
+	ErrMessageTraceInformationMissing = "trace information missing in request"
+	ErrMessageRequestCanceled         = "request canceled"
+	ErrMessageRequestTimeout          = "request timeout"
+	ErrMessageRequestAborted          = "request aborted"
+	ErrMessageUnimplemented           = "unimplemented"
+	ErrMessageNotFound                = "resource not found"
+	ErrMessageInvalidArgument         = "invalid argument"
+	ErrMessageValidation              = "validation error"
+	ErrMessagePermissionDenied        = "permission denied"
+	ErrMessageResourceConflict        = "resource conflict"
+	ErrMessageInvalidEntity           = "entity validation failed: missing required fields"
+)
+
 // general errors
 var (
-	ErrInternalServerError     = NewCustomError(10001, "internal server error", nil)
-	ErrTraceInformationMissing = NewCustomError(10002, "trace information missing in request", nil)
-	ErrRequestCanceled         = NewCustomError(10003, "request canceled", nil)
-	ErrRequestTimeout          = NewCustomError(10004, "request timeout", nil)
-	ErrRequestAborted          = NewCustomError(10005, "request aborted", nil)
-	ErrUnimplemented           = NewCustomError(10006, "unimplemented", nil)
-	ErrNotFound                = NewCustomError(10007, "resource not found", nil)
-	ErrInvalidArgument         = NewCustomError(10008, "invalid argument", nil)
-	ErrValidation              = NewCustomError(10009, "validation error", nil)
-	ErrPermissionDenied        = NewCustomError(10010, "permission denied", nil)
-	ErrResourceConflict        = NewCustomError(10011, "resource conflict", nil)
+	ErrInternalServerError     = NewCustomError(ErrCodeInternalServerError, ErrMessageInternalServerError, nil)
+	ErrTraceInformationMissing = NewCustomError(ErrCodeTraceInformationMissing, ErrMessageTraceInformationMissing, nil)
+	ErrRequestCanceled         = NewCustomError(ErrCodeRequestCanceled, ErrMessageRequestCanceled, nil)
+	ErrRequestTimeout          = NewCustomError(ErrCodeRequestTimeout, ErrMessageRequestTimeout, nil)
+	ErrRequestAborted          = NewCustomError(ErrCodeRequestAborted, ErrMessageRequestAborted, nil)
+	ErrUnimplemented           = NewCustomError(ErrCodeUnimplemented, ErrMessageUnimplemented, nil)
+	ErrNotFound                = NewCustomError(ErrCodeNotFound, ErrMessageNotFound, nil)
+	ErrInvalidArgument         = NewCustomError(ErrCodeInvalidArgument, ErrMessageInvalidArgument, nil)
+	ErrValidation              = NewCustomError(ErrCodeValidation, ErrMessageValidation, nil)
+	ErrPermissionDenied        = NewCustomError(ErrCodePermissionDenied, ErrMessagePermissionDenied, nil)
+	ErrResourceConflict        = NewCustomError(ErrCodeResourceConflict, ErrMessageResourceConflict, nil)
+	ErrInvalidEntity           = NewCustomError(ErrCodeInvalidEntity, ErrMessageInvalidEntity, nil)
 )
